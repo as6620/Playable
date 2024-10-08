4e409d1ba1ab166250ee0abf8d066837f927008a
as6620 <as6620@bs.amalnet.k12.il>
2024-10-08 08:18:36 +0300

    Guitar

--- src/Guitar.java
+++ src/Guitar.java
@@ -1,2 +1,8 @@
 public class Guitar implements Playable{
+    public void play(){
+        System.out.println("play Guitar");
+    }
+    public void coolFeatureGuitar(){
+        System.out.println("coolFeatureGuitar");
+    }
 }
